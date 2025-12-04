baf8883b749ba45086e16541b486e49e0a158f1e
HectorGonzalez-dev <hector.gonzalez.dev@gmail.com>
2025-12-04 02:03:52 +0100

    day 2 part 2 final optimization

--- Day 1/Day-1_Secret-Entrance_Part-2.py
+++ Day 1/Day-1_Secret-Entrance_Part-2.py
@@ -6,36 +6,25 @@ for line in fichero:
     inputs.append(line.replace("\n",""))
 
 dialPos = 50
-dialMin = 0
-dialMax = 99
+dialRange = 100 # Del 0 al 99
 zeroCounter = 0
 iteracionesTotales = 0
 
 for instruction in inputs:
+    iteracionesTotales = iteracionesTotales + 1
     direction = instruction[:1]
     rotation = int(instruction[1:])
-    while rotation > 100:
-        iteracionesTotales = iteracionesTotales + 1
-        rotation = rotation - 100
-        zeroCounter = zeroCounter + 1
-    if direction == "L":
-        for i in range(rotation):
-            iteracionesTotales = iteracionesTotales + 1
-            if dialPos - 1 == -1:
-                dialPos = dialMax
-            else:
-                dialPos = dialPos - 1
-            if dialPos == 0:
+    zeroCounter = zeroCounter + (rotation // dialRange)
+    previousDialPos = dialPos
+    if rotation % dialRange != 0:
+        if direction == "L":
+            dialPos = (dialPos - rotation) % dialRange
+            if (dialPos > previousDialPos and previousDialPos != 0) or dialPos == 0:
                 zeroCounter = zeroCounter + 1
-    else:
-        for i in range(rotation):
-            iteracionesTotales = iteracionesTotales + 1
-            if dialPos + 1 == 100:
-                dialPos = dialMin
-            else:
-                dialPos = dialPos + 1
-            if dialPos == 0:
+        else:
+            dialPos = (dialPos + rotation) % dialRange
+            if previousDialPos > dialPos or dialPos == 0:
                 zeroCounter = zeroCounter + 1
 
 print("El dial ha pasado",zeroCounter,"veces en por el 0")
-print("Para obtener este resultado se han iterado",iteracionesTotales,"veces") # 218.133
+print("Para obtener este resultado se han iterado",iteracionesTotales,"veces (El minimo posible en este caso)")
